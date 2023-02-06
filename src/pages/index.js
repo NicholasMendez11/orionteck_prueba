217d5ccd90030e8b97e9d5bd94a917e12d71d7fe
Nicholas Mendez <nicholasmeeendez@gmail.com>
2023-02-06 00:58:15 -0400

    Viewport added

--- src/pages/index.js
+++ src/pages/index.js
@@ -22,7 +22,7 @@ export default function Home() {
   return (
     <>
     
-      <div className="w-full sm:px-2">
+      <div className="w-full sm:px-2 m-5">
         <div className="px-4 md:px-10 py-4 md:py-7 bg-gray-100 rounded-tl-lg rounded-tr-lg">
           <div className="sm:flex items-center justify-between">
             <p className="text-base sm:text-lg md:text-xl lg:text-2xl font-bold leading-normal text-gray-800">
